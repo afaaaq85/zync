750fed743009c414ca551213a5b2830a82e4dba1
Afaq Ahmed <afaaaq85@gmail.com>
2024-06-12 17:12:59 +0500

    remove protected route

--- client/src/components/MainStack/MainStack.jsx
+++ client/src/components/MainStack/MainStack.jsx
@@ -4,25 +4,19 @@ import Navbar from "../Navbar/Navbar";
 import Login from "../Auth/Login";
 import Signup from "../Auth/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "../../PrivateRoute";
-import useAuth from "../../hooks/useAuth";
 
 const MainStack = () => {
-    const {userToken} = useAuth();
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
   return (
     <Router>
-      {userToken && <Navbar isOpen={isOpen} toggleSidebar={toggleSidebar} setIsOpen={setIsOpen} />}
+      <Navbar isOpen={isOpen} toggleSidebar={toggleSidebar} setIsOpen={setIsOpen} />
       <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path="/home" element={<Chat />} />
-        </Route>
-
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Chat />} />
         <Route path="*" element={<div className="mt-5 text-center ">Page not found</div>} />
       </Routes>
     </Router>
